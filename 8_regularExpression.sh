d221bf9feb94fe5ea1e0d560af0ab90f1b6e6ef3
david.parra <david.parra@rappi.com>
2024-04-19 19:11:58 -0500

    Scripts interactivos

---
+++ 8_regularExpression.sh
@@ -0,0 +1,32 @@
+# !/bin/bash
+# Programa de ejemplo de uso de Regex
+# Autor: Juan David Herrera
+
+regexNum='^[0-9]{10}$' #valida numeros de longitud 10
+paisRegex='^EC|COL|US$'
+dateRegex='^(19|20)([0-9]{2})(0[1-9]|1[0-2])(0[1-9]|[1-2][0-9]|3[0-1])$' #valida >
+
+echo "Expresiones Regulares"
+read -p "Ingresa un Id: " id
+read -p "Ingresa las Iniciales del Pais: " pais
+read -p "Ingresa una fecha yyyymmdd: " date
+
+#Validamos la informacion
+
+if [[ $id =~ $regexNum ]]; then
+        echo "ID: $id valido"
+else
+        echo "ID: $id invalido"
+fi
+
+if [[ $pais =~ $paisRegex ]]; then
+        echo "Pais: $pais valido"
+else
+        echo "Pais: $pais invalido"
+fi
+
+if [[ $date =~ $dateRegex ]]; then
+        echo "Fecha: $date valida"
+else
+        echo "Fecha: $date invalida"
+fi
